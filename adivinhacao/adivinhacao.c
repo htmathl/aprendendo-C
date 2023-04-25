1caa2c5550d2d70c18c1f573a59769e696804aa0
Matheus <matheus.domingues126@gmail.com>
2023-04-25 13:14:03 -0300

    Aprimorar ifs

--- adivinhacao/adivinhacao.c
+++ adivinhacao/adivinhacao.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+int acerto(int acertou, int maior) {
+    if(acertou) {
+        printf("Parabéns! Voce acertou o número\n");
+        return 0;
+    }
+    printf("Voce errou :(\n");
+    printf("O número secreto é ");
+    if(maior) {
+        printf("menor\n");
+    } else {
+        printf("maior\n");
+    }
+}
+
 int main() {
     printf("*******************************************\n");
     printf("* Bem vindo ao nosso jogo de adivinhação! *\n");
@@ -13,15 +27,10 @@ int main() {
 
     printf("Seu chute foi %d \n", chute);
 
-    if(chute == num) {
-        printf("Parabrns! Voce acertou o numero");
-    } else if(chute > num) {
-        printf("Voce errou :(\n");
-        printf("O número é menor");
-    } else {
-        printf("Voce errou :(\n");
-        printf("O número é maior");
-    }
+    int acertou = chute == num;
+    int maior = chute > num;
+
+    acerto(acertou, maior);
 
     return 0;
 }
